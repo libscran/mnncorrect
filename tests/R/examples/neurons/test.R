90db33ecdfff22c18d0dd12e7e6b48a6ab48aa8b
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2021-11-18 23:41:40 -0800

    Added neuronal test, reorganized examples.

--- tests/R/examples/neurons/test.R
+++ tests/R/examples/neurons/test.R
@@ -1,16 +1,16 @@
 ########################
 # Setting up the datasets.
 
-library(TENxPBMCData)
+library(scRNAseq)
 
-sce3k <- TENxPBMCData("pbmc3k")
-sce4k <- TENxPBMCData("pbmc4k")
-common <- intersect(rownames(sce3k), rownames(sce4k))
+sce.z <- ZeiselBrainData()
+sce.t <- TasicBrainData()
+common <- intersect(rownames(sce.z), rownames(sce.t))
 
-sce3k <- sce3k[common,]
-sce4k <- sce4k[common,]
-x0 <- cbind(assay(sce3k), assay(sce4k))
-block <- rep(c("3k", "4k"), c(ncol(sce3k), ncol(sce4k)))
+sce.z <- sce.z[common,]
+sce.t <- sce.t[common,]
+x0 <- cbind(assay(sce.z), assay(sce.t))
+block <- rep(c("zeisel", "tasic"), c(ncol(sce.z), ncol(sce.t)))
 
 saveRDS(list(x0, block), file="whee.rds")
 # X <- readRDS("whee.rds"); x0 <- X[[1]]; block <- X[[2]]
@@ -27,24 +27,28 @@ qc.discard <- qc.filters$filters$overall
 x <- filterCells.chan(x, qc.discard)
 
 sf <- qc.metrics$sums[!qc.discard]
-x <- logNormCounts.chan(x, sf, batch=block)
+plock <- block[!qc.discard]
+x <- logNormCounts.chan(x, sf, batch=plock)
 
-variances <- modelGeneVar.chan(x, batch=block, span = 0.4, num.threads=1)
+variances <- modelGeneVar.chan(x, batch=plock, span = 0.4, num.threads=1)
 keep <- rank(-variances$statistics$residuals, ties.method="first") <= 4000
 
-pca <- runPCA.chan(x, num.comp=25, subset=keep, num.threads=1, batch=block, batch.method="weight")
+pca <- runPCA.chan(x, num.comp=25, subset=keep, num.threads=1, batch=plock, batch.method="weight")
 pcs <- pca$components
 
 #######################
 # Merging method starts here.
 
-plock <- block[!qc.discard]
-y3k <- pcs[,plock == "3k"]
-y4k <- pcs[,plock == "4k"]
+yz <- pcs[,plock == "zeisel"]
+yt <- pcs[,plock == "tasic"]
 
 library(mnncorrect.ref)
-corrected.3k <- mnncorrect.ref(y4k, y3k, k=15)
-total <- cbind(corrected.3k, y4k)
+corrected.t <- mnncorrect.ref(yz, yt, k=50)
+total <- cbind(yz, corrected.t)
 out <- runTSNE.chan(total)
+
 plot(out[,1], out[,2], col=factor(plock))
 
+
+clusters <- clusterKmeans.chan(yz, 20)
+plot(out[plock=="zeisel",1], out[plock=="zeisel",2], col=clusters$clusters)
--- tests/R/examples/pancreas/test.R
+++ tests/R/examples/pancreas/test.R
@@ -7,7 +7,7 @@ combined <- cbind(assay(sceG)[common,], assay(sceM)[common,])
 batch <- rep(c("Grun", "Muraro"), c(ncol(sceG), ncol(sceM)))
 
 saveRDS(list(combined, batch), file="args.rds")
-#X <- readRDS("args.rds"); combined <- X[[1]]; block <- X[[2]]
+# X <- readRDS("args.rds"); combined <- X[[1]]; block <- X[[2]]
 
 ########################
 # Preamble of scran.chan::quickMergedAnalysis
@@ -22,18 +22,18 @@ qc.discard <- qc.filters$filters$overall
 x <- filterCells.chan(x, qc.discard)
 
 sf <- qc.metrics$sums[!qc.discard]
-x <- logNormCounts.chan(x, sf, batch=block)
+plock <- block[!qc.discard]
+x <- logNormCounts.chan(x, sf, batch=plock)
 
-variances <- modelGeneVar.chan(x, batch=block, span = 0.4, num.threads=1)
+variances <- modelGeneVar.chan(x, batch=plock, span = 0.4, num.threads=1)
 keep <- rank(-variances$statistics$residuals, ties.method="first") <= 4000
 
-pca <- runPCA.chan(x, num.comp=25, subset=keep, num.threads=1, batch=block, batch.method="weight")
+pca <- runPCA.chan(x, num.comp=25, subset=keep, num.threads=1, batch=plock, batch.method="weight")
 pcs <- pca$components
 
 #######################
 # Merging method starts here.
 
-plock <- block[!qc.discard]
 y.g <- pcs[,plock == "Grun"]
 y.m <- pcs[,plock == "Muraro"]
 
