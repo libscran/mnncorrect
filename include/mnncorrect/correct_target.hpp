e7e0ef2c2512e37aaaca7be2505391555fe08bf0
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2021-11-21 16:31:35 -0800

    Removed all the extraneous print statements.

--- include/mnncorrect/correct_target.hpp
+++ include/mnncorrect/correct_target.hpp
@@ -97,20 +97,10 @@ void correct_target(
     std::vector<Float> buffer_target(uniq_target.size() * ndim);
     auto mnn_target = identify_closest_mnn(ndim, ntarget, target, uniq_target, bfun, k, buffer_target.data());
     Float limit_closest_target = limit_from_closest_distances(mnn_target, nmads);
-    std::cout << limit_closest_ref << "\t" << limit_closest_target << std::endl;
 
     // Computing the centers of mass, stored in the buffers.
     compute_center_of_mass(ndim, uniq_ref.size(), mnn_ref, ref, buffer_ref.data(), robust_iterations, robust_trim, limit_closest_ref);
     compute_center_of_mass(ndim, uniq_target.size(), mnn_target, target, buffer_target.data(), robust_iterations, robust_trim, limit_closest_target);
-
-    std::cout << "REF:" << std::endl;
-    for (int d = 0; d < ndim; ++d) {
-        std::cout << "\t" << buffer_ref[d] << std::endl;
-    }
-    std::cout << "TARGET:" << std::endl;
-    for (int d = 0; d < ndim; ++d) {
-        std::cout << "\t" << buffer_target[d] << std::endl;
-    }
 
     // Computing the correction vector for each target point, 
     // And then applying it to the target data.
