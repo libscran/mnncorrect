6c4df8d3e68c74bc2a3276f0f482f4230de9693d
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-08-07 13:09:02 -0700

    Added umbrella header, updated doxyfile.

---
+++ include/mnncorrect/mnncorrect.hpp
@@ -0,0 +1,19 @@
+#ifndef MNNCORRECT_HPP
+#define MNNCORRECT_HPP
+
+#include "Options.hpp"
+#include "ReferencePolicy.hpp"
+#include "compute.hpp"
+
+/**
+ * @file mnncorrect.hpp
+ * @brief Batch correction with mutual nearest neighbors.
+ */
+
+/**
+ * @namespace mnncorrect
+ * @brief Batch correction with mutual nearest neighbors.
+ */
+namespace mnncorrect {}
+
+#endif
